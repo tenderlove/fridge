006ee0948218543f107cfed2a86fb650c48ba3c5
Aaron Patterson <aaron.patterson@gmail.com>
2012-07-09 11:16:49 -0700

    writing values to the database

--- reader.scm
+++ reader.scm
@@ -1,4 +1,5 @@
 (use posix)
+(require-extension sqlite3)
 
 (define (make-serialport file)
   (let ((fileno
@@ -21,14 +22,39 @@
 
 ; Convert the list of pairs to 16 bit ints and divide by 10
 (define (decode s)
-  (map (lambda (pair)
-         (let ((l (car pair)) (r (cadr pair)))
-           (/ (+ (arithmetic-shift l 8) r) 10)))
-       (split (map char->integer (string->list s)))))
+  (if (eq? 8 (string-length s))
+    (map (lambda (pair)
+           (let ((l (car pair)) (r (cadr pair)))
+             (/ (+ (arithmetic-shift l 8) r) 10)))
+         (split (map char->integer (string->list s))))
+    (list 'fail (map char->integer (string->list s)) (string-length s))))
+
+(define (create-tables db)
+  (execute db "CREATE TABLE IF NOT EXISTS measurements
+  (id INTEGER PRIMARY KEY AUTOINCREMENT,
+   humidity INTEGER NOT NULL,
+   temp INTEGER NOT NULL,
+   ambient_temp INTEGER NOT NULL,
+   ambient_humidity INTEGER NOT NULL,
+   created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+   )"))
+
+(define db (open-database "sausage.sqlite3"))
+
+(create-tables db)
+
+(define stmt (prepare db "INSERT INTO measurements (humidity, temp, ambient_humidity, ambient_temp) VALUES (?, ?, ?, ?)"))
+
+(define (save-sausage)
+  (let ((data (decode (read-line))))
+       (if (not (eq? 'fail (car data)))
+           (execute stmt (car data) (cadr data) (caddr data) (cadddr data)))
+       data))
 
 (define (read-sausage) (decode (read-line)))
 
-(define (print-sausage) (port-for-each print read-sausage))
+(define (print-sausage) (port-for-each print save-sausage))
+; (define (print-sausage) (port-for-each print read-sausage))
 
 (define sausage-port (make-serialport "/dev/tty.uart-AEFF467AF9D70234"))
 
